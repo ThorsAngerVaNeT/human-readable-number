ae9d72d9c167c898f43cee8f9cfe2651872bb063
Vadzim Antonau <vanet@tut.by>
2021-12-30 13:12:03 +0300

    feat: add toReadable func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-module.exports = function toReadable (number) {
-  
+const numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+    "twenty",
+];
+const decades = [
+    "ten",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+
+
+function getWordsBelow100 (number){
+    return (number <= 20 ? numbers[number] : (decades[Math.floor(number / 10)-1] + ((number % 10 === 0) ? '' : ' ' + numbers[number % 10])));
 }
+
+module.exports = function toReadable(number) {
+    return number < 100 ? getWordsBelow100(number) : (numbers[Math.floor(number / 100)] + ' hundred' + ((number % 100 === 0) ? '' : ' ' + getWordsBelow100(number % 100)));
+};
